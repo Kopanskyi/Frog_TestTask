4d26b0e4ae9ccf9aab5788c22ee5a36c0497fe6b
Taras Kopanskyi <tkopanskyi@gmail.com>
2020-11-06 13:23:57 +0200

    Add project files.

---
+++ Frog_TestTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Frog_TestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
