0103a4c163d605035f474f078e437ef1dee3c383
Taras Kopanskyi <tkopanskyi@gmail.com>
2020-11-09 11:05:04 +0200

    Added GenerateArr and FindShortestWay methods

--- Frog_TestTask/Frog.cs
+++ Frog_TestTask/Frog.cs
@@ -8,89 +8,89 @@ namespace Frog_TestTask
     class Frog
     {
         public Point CurrentPosition { get; set; }
-        public Point Target { get; set; }
-        public List<Point> TreePositions { get; set; }
+        //public Point Target { get; set; }
+        //public List<Point> TreePositions { get; set; }
 
         public Frog()
         {
             CurrentPosition = new Point 
             {
-                Coordinate_X = 10,
-                Coordinate_Y = 6
+                Coordinate_X = 11,
+                Coordinate_Y = 7
             };
 
-            Target = new Point
-            {
-                Coordinate_X = 8,
-                Coordinate_Y = 9
-            };
+            //Target = new Point
+            //{
+            //    Coordinate_X = 8,
+            //    Coordinate_Y = 9
+            //};
 
-            TreePositions = new List<Point>
-            {
-                new Point
-                {
-                    Coordinate_X = 4,
-                    Coordinate_Y = 7
-                },
-                new Point
-                {
-                    Coordinate_X = 13,
-                    Coordinate_Y = 8
-                }
-            };
+            //TreePositions = new List<Point>
+            //{
+            //    new Point
+            //    {
+            //        Coordinate_X = 4,
+            //        Coordinate_Y = 7
+            //    },
+            //    new Point
+            //    {
+            //        Coordinate_X = 13,
+            //        Coordinate_Y = 8
+            //    }
+            //};
         }
 
-        public bool Move(Point moveTo)
-        {
-            var canMove = moveTo.Coordinate_X >= 0
-                          && moveTo.Coordinate_X <= 16
-                          && moveTo.Coordinate_Y >= 0
-                          && moveTo.Coordinate_Y <= 10
-                          && TreePositions.All(p => p.Coordinate_X != moveTo.Coordinate_X && p.Coordinate_Y != moveTo.Coordinate_Y);
+        //public bool Move(Point moveTo)
+        //{
+        //    var canMove = moveTo.Coordinate_X >= 0
+        //                  && moveTo.Coordinate_X <= 16
+        //                  && moveTo.Coordinate_Y >= 0
+        //                  && moveTo.Coordinate_Y <= 10
+        //                  && TreePositions.All(p => p.Coordinate_X != moveTo.Coordinate_X && p.Coordinate_Y != moveTo.Coordinate_Y);
 
-            if (!canMove)
-            {
-                return false;
-            }
+        //    if (!canMove)
+        //    {
+        //        return false;
+        //    }
 
-            var differenceBetweenXCoordinates = 0;
-            var newXCoordinateShouldBe = CurrentPosition.Coordinate_X;
+        //    var differenceBetweenXCoordinates = 0;
+        //    var newXCoordinateShouldBe = CurrentPosition.Coordinate_X;
 
-            // Defining the difference between old and new X coordinate
-            if (moveTo.Coordinate_Y == CurrentPosition.Coordinate_Y)
-            {
-                differenceBetweenXCoordinates = 3;
-            }
-            else if (moveTo.Coordinate_Y == CurrentPosition.Coordinate_Y + 1 ||
-                    moveTo.Coordinate_Y == CurrentPosition.Coordinate_Y - 1)
-            {
-                differenceBetweenXCoordinates = 2;
-            }
-            else if (moveTo.Coordinate_Y == CurrentPosition.Coordinate_Y + 2 ||
-                    moveTo.Coordinate_Y == CurrentPosition.Coordinate_Y - 2)
-            {
-                differenceBetweenXCoordinates = 1;
-            }
+        //    // Defining the difference between old and new X coordinate
+        //    if (moveTo.Coordinate_Y == CurrentPosition.Coordinate_Y)
+        //    {
+        //        differenceBetweenXCoordinates = 3;
+        //    }
+        //    else if (moveTo.Coordinate_Y == CurrentPosition.Coordinate_Y + 1 ||
+        //            moveTo.Coordinate_Y == CurrentPosition.Coordinate_Y - 1)
+        //    {
+        //        differenceBetweenXCoordinates = 2;
+        //    }
+        //    else if (moveTo.Coordinate_Y == CurrentPosition.Coordinate_Y + 2 ||
+        //            moveTo.Coordinate_Y == CurrentPosition.Coordinate_Y - 2)
+        //    {
+        //        differenceBetweenXCoordinates = 1;
+        //    }
 
-            // Defining what new X coordinate should be
-            for (int i = 0; i < differenceBetweenXCoordinates; i++)
-            {
-                newXCoordinateShouldBe++;
+        //    // Defining what new X coordinate should be
+        //    for (int i = 0; i < differenceBetweenXCoordinates; i++)
+        //    {
+        //        newXCoordinateShouldBe++;
 
-                if (newXCoordinateShouldBe > 16)
-                {
-                    newXCoordinateShouldBe = 1;
-                }
-            }
+        //        if (newXCoordinateShouldBe > 16)
+        //        {
+        //            newXCoordinateShouldBe = 1;
+        //        }
+        //    }
 
-            if (canMove && differenceBetweenXCoordinates != 0 && 
-                moveTo.Coordinate_X == newXCoordinateShouldBe)
-            {
-                CurrentPosition = moveTo;
-                return true;
-            }           
+        //    if (canMove && differenceBetweenXCoordinates != 0 && 
+        //        moveTo.Coordinate_X == newXCoordinateShouldBe)
+        //    {
+        //        CurrentPosition = moveTo;
+        //        return true;
+        //    }           
 
-            return false;
-        }
+        //    return false;
+        //}
     }
 }
--- Frog_TestTask/Program.cs
+++ Frog_TestTask/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Frog_TestTask
 {
@@ -6,7 +8,116 @@ namespace Frog_TestTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var treePositions = new List<Point>
+            {
+                new Point
+                {
+                    Coordinate_X = 5,
+                    Coordinate_Y = 8
+                },
+                new Point
+                {
+                    Coordinate_X = 14,
+                    Coordinate_Y = 9
+                }
+            };
+
+            var target = new Point
+            {
+                Coordinate_X = 9,
+                Coordinate_Y = 10
+            };
+
+            var frog = new Frog(); 
+
+            var arr = GenerateArr(treePositions, target, frog);
+            ShowArr(arr);
+
+            Console.ReadKey();
+        }
+
+        public static int FindShortestWay(List<Point> treePositions, Point target, Frog frog)
+        {
+            var currentPosition = new Point 
+            { 
+                Coordinate_X = frog.CurrentPosition.Coordinate_X,
+                Coordinate_Y = frog.CurrentPosition.Coordinate_Y
+            };
+
+            var wayToTargetByXcoordinate = 0;
+            var wayToTargetByYcoordinate = 0;
+
+            if (currentPosition.Coordinate_X > target.Coordinate_X)
+            {
+                wayToTargetByXcoordinate = 16 - currentPosition.Coordinate_X + target.Coordinate_X;
+                
+            }
+            if (currentPosition.Coordinate_X < target.Coordinate_X)
+            {
+                wayToTargetByXcoordinate = currentPosition.Coordinate_X - target.Coordinate_X;
+            }
+            if (currentPosition.Coordinate_Y > target.Coordinate_Y)
+            {
+                wayToTargetByYcoordinate = currentPosition.Coordinate_Y - target.Coordinate_Y;
+            }
+            if (currentPosition.Coordinate_Y < target.Coordinate_Y)
+            {
+                wayToTargetByYcoordinate = target.Coordinate_Y - currentPosition.Coordinate_Y;
+            }
+
+
+
+
+
+
+
+            return 0;
+        }
+
+        public static string[][] GenerateArr(List<Point> treePositions, Point target, Frog frog)
+        {
+            var arr = new string[10][];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new string[16];
+
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+
+                    if (treePositions.Any(p => p.Coordinate_X - 1 == j && p.Coordinate_Y - 1 == i))
+                    {
+                        arr[i][j] = "T";
+                    }
+                    else if (j == target.Coordinate_X - 1 && i == target.Coordinate_Y - 1)
+                    {
+                        arr[i][j] = "*";
+                    }
+                    else if (j == frog.CurrentPosition.Coordinate_X - 1 && i == frog.CurrentPosition.Coordinate_Y - 1)
+                    {
+                        arr[i][j] = "F";
+                    }
+                    else
+                    {
+                        arr[i][j] = "_";
+                    }
+                }
+            }
+
+            return arr;
+        }
+
+        public static void ShowArr(string[][] arr)
+        {
+            foreach (var row in arr)
+            {
+                foreach (var item in row)
+                {
+                    Console.Write(item);
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
